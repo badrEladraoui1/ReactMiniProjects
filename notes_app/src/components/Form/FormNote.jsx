9b09f18f1a26e6c96f0ccaa0d164b2bf40de714e
badrEladraoui1 <eladraoui.badr@gmail.com>
2023-12-31 11:33:43 +0100

    deploying on netlify

--- notes_app/src/components/Form/FormNote.jsx
+++ notes_app/src/components/Form/FormNote.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-import Button from "../Buttons/Button";
-
 import styles from "../../styles/Form/FormNote.module.css";
 
 const FormNote = ({ onAddNewNoteToNotes }) => {
   const [newNote, setNewNote] = useState("");
 
-  const handleSumbit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     if (newNote === "") return "";
     onAddNewNoteToNotes(newNote);
@@ -16,17 +14,19 @@ const FormNote = ({ onAddNewNoteToNotes }) => {
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSumbit}>
-      <h2 htmlFor="note"> your note : </h2>
-      <input
-        value={newNote}
-        onChange={(e) => setNewNote(e.target.value)}
-        id="note"
-        type="text"
-      />
-      <Button className={styles.button} type="submit">
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.insideForm}>
+        <h2 htmlFor="note"> your note : </h2>
+        <input
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+          id="note"
+          type="text"
+        />
+      </div>
+      <button className={styles.button} type="submit">
         Add Note
-      </Button>
+      </button>
     </form>
   );
 };
