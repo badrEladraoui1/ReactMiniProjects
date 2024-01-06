bc4d8c50c2615edb6263a3100150c187b924750f
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-06 23:17:33 +0100

    datamuse api

--- datamuse_api/src/App.jsx
+++ datamuse_api/src/App.jsx
@@ -11,53 +11,63 @@ const App = () => {
   const [synonyms, setSynonyms] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-  const [isNotEmpty, setIsNotEmpty] = useState();
+  const [isNotEmpty, setIsNotEmpty] = useState(false);
 
   const searchedInput = (value) => {
     setInputPassed(value);
   };
 
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     setIsLoading(true);
     const fetchingData = async () => {
       try {
-        const response = await fetch(`${baseUrl}${inputPassed}`);
+        const response = await fetch(`${baseUrl}${inputPassed}`, { signal });
         const data = await response.json();
+
         const gettingWords = () => {
           if (data) {
-            setIsNotEmpty(true);
+            // setIsNotEmpty(true);
             const words = data.map((dataItem) => dataItem.word);
-            let wordsFiveWords = [];
+            if (words.length > 0) setIsNotEmpty(true);
+            let SevenWords = [];
             for (let i = 0; i < 7; i++) {
-              wordsFiveWords.push(
-                words[Math.floor(Math.random() * words.length)]
-              );
+              SevenWords.push(words[Math.floor(Math.random() * words.length)]);
             }
-            setSynonyms(wordsFiveWords);
+            setSynonyms(SevenWords);
           } else setIsNotEmpty(false);
         };
         gettingWords();
       } catch (e) {
+        if (e.name === "AbortError") console.log("Aborted : ", e);
         setError(e);
       } finally {
         setIsLoading(false);
       }
     };
     fetchingData();
+
+    return () => {
+      // controller.abort();
+    };
   }, [inputPassed]);
 
-  if (error) return <h1>Loading...</h1>;
+  if (error) return <h1>Something Went Wrong !!!</h1>;
 
   return (
     <div className="app">
       <h1>Search for any word</h1>
       <Form onSearchedInput={searchedInput} />
-      {isNotEmpty && !isLoading ? (
-        <Synonyms synonyms={synonyms} />
-      ) : isLoading ? (
-        <h1>Loading...</h1>
+      {!isLoading ? (
+        isNotEmpty ? (
+          <Synonyms synonyms={synonyms} />
+        ) : (
+          <h1>No synonyms found :(</h1>
+        )
       ) : (
-        <h1>No results found</h1>
+        <h1>Loading</h1>
       )}
     </div>
   );
