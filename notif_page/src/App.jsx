1db2af695eee1ae8d2c2ec308dda78e35079d8c3
badrEladraoui1 <eladraoui.badr@gmail.com>
2024-01-13 18:06:39 +0100

    styling

--- notif_page/src/App.jsx
+++ notif_page/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 import Notifications from "./components/Notifications";
 
 const App = () => {
+  const [notifications, setnotifications] = useState(3);
+
+  const getNotifNumber = (number) => setnotifications(number);
+
   return (
-    <div className="container">
+    <div className="bigContainer">
+      <div className="notificationText">
+        <div className="flexNotifs">
+          <h1>Notifications</h1>
+          <h3 className="howManyNotifs">{notifications}</h3>
+        </div>
+        <h4 className="MarkAllAsRead blueAndPointer">Mark all as read</h4>
+      </div>
       <Notifications />
     </div>
   );
--- notif_page/src/components/ItemNotif.jsx
+++ notif_page/src/components/ItemNotif.jsx
@@ -4,13 +4,37 @@
 
 const ItemNotif = ({ notif }) => {
   return (
-    <div>
-      <h1 style={{ color: "red" }}>{notif.name}</h1>
-      <h2 style={{ color: "red" }}>{notif.action}</h2>
-      <h3 style={{ color: "red" }}>{notif.time}</h3>
-      <h4 style={{ color: "red" }}>{notif.msg}</h4>
-      <img src={notif.img}></img>
-      <hr />
+    <div className={`notificationItem ${!notif.read ? "blueBg" : ""}`}>
+      <div className="importantInfos">
+        <img className="image" src={notif.img_prim}></img>
+        <div className="mainInfos">
+          <div className="specificInfos">
+            <h3 className="blueAndPointer">{notif.name}</h3>
+            <p className="action">{notif.action}</p>
+            {notif.onPost ? (
+              <div className="notRead">
+                <span className="reason PostText blueAndPointer">
+                  {notif.onPost}
+                </span>
+                {!notif.read && <div className="redDot"></div>}
+              </div>
+            ) : (
+              <div className="notRead">
+                <span className="reason groupText blueAndPointer">
+                  {notif.onGroup}
+                </span>
+                {!notif.read && <div className="redDot"></div>}
+              </div>
+            )}
+          </div>
+          <p>{notif.time}</p>
+          {notif.msg && <p className="privateMessage pOnHover">{notif.msg}</p>}
+        </div>
+        {notif.img_sec && (
+          <img className="image imageHelper" src={notif.img_sec}></img>
+        )}
+      </div>
+      <div className="time_msgInfos"></div>
     </div>
   );
 };
